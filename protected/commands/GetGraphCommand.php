fd8e95c97116a0db90d5d5c8f8453b633b6a8643
Leonid Minderov <leonidminderov@MacBook-Pro-Leonid.local>
2018-01-10 18:35:53 +0500

    asd

--- protected/commands/GetGraphCommand.php
+++ protected/commands/GetGraphCommand.php
@@ -9,20 +9,24 @@ class GetGraphCommand extends CConsoleCommand {
     	$time_now = date('Y-m-d H:i:s');
     	// $value = rand(15000, 25000);
 
-        $SQL="SELECT * FROM graph ORDER BY coord_x LIMIT 1";
+        $SQL="SELECT * FROM graph ORDER BY coord_x DESC LIMIT 1";
         $connection=Yii::app()->db; 
         $command=$connection->createCommand($SQL);
-        $last_y=$connection->createCommand($SQL)->queryRow()['coord_y'];
+        $row=$connection->createCommand($SQL)->queryRow();
+        $last_y = $row['coord_y'];
 
+        // var_dump($time_now);
+        // var_dump($row['coord_x']);
+        if( strtotime($time_now) != strtotime($row['coord_x']) )
+        {
+            // var_dump('create new');
+            $value = rand( $last_y * (1-$percents) , $last_y * (1+$percents));
+            $SQL="INSERT INTO graph (id_currency, id_currency_to, coord_x, coord_y) VALUES ({$id_currency}, {$id_currency_to}, '{$time_now}', '{$value}')";
+            $command=$connection->createCommand($SQL);
+            $command->execute(); // execute the non-query SQL
+        }
 
-        $value = rand( $last_y * (1-$percents) , $last_y * (1+$percents));
-
-      
-
-        $SQL="INSERT INTO graph (id_currency, id_currency_to, coord_x, coord_y) VALUES ({$id_currency}, {$id_currency_to}, '{$time_now}', '{$value}')";
-		$command=$connection->createCommand($SQL);
-		
-		$command->execute(); // execute the non-query SQL
+        
 
 
     }
